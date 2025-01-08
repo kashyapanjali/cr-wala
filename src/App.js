7a4047192ab7acfbbd71828a515b121af152223d
kashyapanjali <anjalikashyap9608@gmail.com>
2025-01-08 16:03:11 +0530

    file changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import Packages from "./components/Packages";
+import Appointment from "./components/Appointment";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeroSection />
+      <Packages />
+      <Appointment />
+      <Footer />
     </div>
   );
 }
